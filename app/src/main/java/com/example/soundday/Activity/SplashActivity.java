adca9db3ceafbcc2154a3024488104cb1527a758
jeongbbn <54277396+jeongbbn@users.noreply.github.com>
2021-01-27 16:20:02 +0900

    발표 전 디자인 최종 적용

--- app/src/main/java/com/example/soundday/Activity/SplashActivity.java
+++ app/src/main/java/com/example/soundday/Activity/SplashActivity.java
@@ -3,8 +3,10 @@ package com.example.soundday.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 import com.example.soundday.R;
 
@@ -16,6 +18,8 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
         //android:theme="@style/SplashTheme"
 
+        statusbarTransparent();
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -23,5 +27,14 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         }, 1000);
+    }
+
+    private void statusbarTransparent() {
+        //style AppTheme에 <item name="android:statusBarColor">@android:color/transparent</item>
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
     }
 }
